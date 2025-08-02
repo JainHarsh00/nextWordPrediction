b4e91399ab6ccaa5732bf6784180209a5bff1026
Harsh Jain <jainharsh4740@gmail.com>
2025-08-02 12:45:35 +0530

    fixed error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 st.set_page_config(page_title="Next Word Predictor", page_icon="🔮")
 
 # Load the LSTM Model
-model = load_model("next_word_lstm.h5")
+model = load_model("next_word_lstm.keras")
 
 # Load the tokenizer
 with open('tokenizer.pickle', 'rb') as handle:
